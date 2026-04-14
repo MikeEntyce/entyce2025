d2ad8a0000a88f9214947ce45125f49753942de6
Mike <mike@entyce-creative.com>
2026-04-14 13:02:02 +0100

    updates

--- wp-content/themes/hello-theme-child-master/functions-custom.php
+++ wp-content/themes/hello-theme-child-master/functions-custom.php
@@ -4,3 +4,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+function enqueue_nice_select_assets() {
+    // CSS
+    wp_enqueue_style( 'nice-select-css', get_stylesheet_directory_uri() . '/css/nice-select.css' );
+    wp_enqueue_style( 'splitting-css', get_stylesheet_directory_uri() . '/css/splitting.css' );
+
+    // JS (depends on jQuery)
+    wp_enqueue_script( 'nice-select-js', get_stylesheet_directory_uri() . '/js/jquery.nice-select.min.js', array('jquery'), null, true );
+
+    // Init script
+    wp_add_inline_script( 'nice-select-js', 'jQuery(document).ready(function($){ $("select").niceSelect(); });' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_nice_select_assets' );
--- wp-content/themes/hello-theme-child-master/js/custom.js
+++ wp-content/themes/hello-theme-child-master/js/custom.js
@@ -1,7 +1,19 @@
 jQuery(document).ready(function () {
 
+
+document.querySelectorAll('.uc_logo_marquee img').forEach(img => {
+  function setWidth() {
+    img.style.width = (img.naturalWidth / 2) + 'px';
+  }
+  if (img.complete) {
+    setWidth();
+  } else {
+    img.addEventListener('load', setWidth);
+  }
+});
     
 
 
-
 });
+
+
